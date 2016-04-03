4579d3aa320e84dd2358d4b5478cc81e2a7c18df
Philippe MILINK -- Debian <philippe.milink@gmx.fr>
2016-04-03 19:20:40 +0200

    Fix: Source code should not contain TODO comments

--- web/js/jquery.js
+++ web/js/jquery.js
@@ -3713,8 +3713,6 @@ jQuery.extend({
 		data_user.remove( elem, name );
 	},
 
-	// TODO: Now that all calls to _data and _removeData have been replaced
-	// with direct calls to data_priv methods, these can be deprecated.
 	_data: function( elem, name, data ) {
 		return data_priv.access( elem, name, data );
 	},
